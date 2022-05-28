9b098a9defcbe82d95773bf22aa7c7e20e32f02c
Tewodros <tyimers@gmail.com>
2022-05-28 11:49:28 -0400

    modifing product model

--- api/models/product.js
+++ api/models/product.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose');
 
-const productSchema = mongoose.Schema({
+const productSchema =new  mongoose.Schema({
     // _id: mongoose.Schema.Types.ObjectId,
     name: { type: String, required: true },
     price: { type: Number, required: true },
-    productImage: { type: String, required: true }
+    productImage: { type: String, required: true },
+    isAvailable:Boolean
+    
 });
 
 module.exports = mongoose.model('Product', productSchema);
